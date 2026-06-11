aba3529aacbcb95f3bfc7b7402f2a48e94b92dbd
Muhammad <mu7ammad.1.oxo@gmail.com>
2026-06-11 19:45:56 +0300

    create auth service library

---
+++ projects/auth-library/src/lib/services/auth.service.ts
@@ -0,0 +1,46 @@
+import { inject, Injectable, Service } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
+import { AuthEndPoints } from '../enums/AuthEndPoints';
+import { AuthAdapt } from './adaptor/auth-adapt.service';
+import { ConfirmOtpREQ, ConfirmOtpRES, RegisterREQ, RegisterRES, SendOtpToEmailREQ, SendOtpToEmailRES } from '../interfaces/register';
+import { AuthAPI } from '../base/AuthAPI';
+import { BRegisterStep1_2, BRegisterStep3 } from '../interfaces/back-interfaces/b-register';
+import { LoginREQ, LoginRES } from '../interfaces/login';
+import { BLogin } from '../interfaces/back-interfaces/b-login';
+import { RequestPasswordResetREQ, ForgotPasswordRES, ResetPasswordREQ } from '../interfaces/forgotpass';
+import { BForgotpass } from '../interfaces/back-interfaces/b-forgotpass';
+
+@Service()
+export class AuthService implements AuthAPI {
+  private readonly _httpClient = inject(HttpClient)
+  private readonly _authAdapt = inject(AuthAdapt)
+  private readonly _authEndPoints = inject(AuthEndPoints)
+
+  SendOtpToEmail(data: SendOtpToEmailREQ): Observable<SendOtpToEmailRES | string> {
+    return this._httpClient.post<BRegisterStep1_2>(this._authEndPoints.SendOtpToEmail, data)
+      .pipe(map((res: BRegisterStep1_2) => this._authAdapt.OtpStepsAdapt(res)));
+  }
+  ConfirmOtp(data: ConfirmOtpREQ): Observable<ConfirmOtpRES | string> {
+    return this._httpClient.post<BRegisterStep1_2>(this._authEndPoints.ConfirmOtp, data)
+      .pipe(map((res: BRegisterStep1_2) => this._authAdapt.OtpStepsAdapt(res)));
+  }
+  Register(data: RegisterREQ): Observable<RegisterRES | string> {
+    return this._httpClient.post<BRegisterStep3>(this._authEndPoints.Register, data)
+      .pipe(map((res: BRegisterStep3) => this._authAdapt.RegisterAdapt(res)));
+  }
+
+  Login(data: LoginREQ): Observable<LoginRES | string> {
+    return this._httpClient.post<BLogin>(this._authEndPoints.Login, data)
+      .pipe(map((res: BLogin) => this._authAdapt.LoginAdapt(res)));
+  }
+
+  RequestPasswordReset(data: RequestPasswordResetREQ): Observable<ForgotPasswordRES | string> {
+    return this._httpClient.post<BForgotpass>(this._authEndPoints.RequestPasswordReset, data)
+      .pipe(map((res) => this._authAdapt.ForgotPassAdapt(res)));
+  }
+  ResetPassword(data: ResetPasswordREQ): Observable<ForgotPasswordRES | string> {
+    return this._httpClient.post<BForgotpass>(this._authEndPoints.ResetPassword, data)
+      .pipe(map((res) => this._authAdapt.ForgotPassAdapt(res)));
+  }
+}
---
+++ projects/auth-library/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of auth-library
+ */
+
+export * from './lib/auth-library';
