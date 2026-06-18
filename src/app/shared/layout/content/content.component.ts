2d7e2418d1930e0b43a360317316d92749f866f7
MahmoudSayedDeve <76589982+MahmoudSayed889@users.noreply.github.com>
2026-06-18 19:23:15 +0300

    add-footer

--- src/app/shared/layout/content/content.component.ts
+++ src/app/shared/layout/content/content.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-content',
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss',
