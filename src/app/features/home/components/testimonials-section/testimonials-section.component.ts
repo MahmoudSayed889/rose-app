c055a82c23dbafe65c9765d44718d285630c6d00
MahmoudSayedDeve <76589982+MahmoudSayed889@users.noreply.github.com>
2026-06-30 07:24:51 +0300

    feature/testimonials-section

---
+++ src/app/features/home/components/testimonials-section/testimonials-section.component.ts
@@ -0,0 +1,42 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TestimonialsService } from './services/testimonials.service';
+import { ExternalParams } from '../../../../shared/models/external-params';
+import { Testimonial, TestimonialsList } from './models/testimonial';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-testimonials-section',
+  imports: [
+    RatingModule,
+    FormsModule,
+    DatePipe
+  ],
+  templateUrl: './testimonials-section.component.html',
+  styleUrl: './testimonials-section.component.scss',
+})
+export class TestimonialsSectionComponent implements OnInit {
+
+  private _testimonialsService = inject(TestimonialsService)
+
+  testimonials = signal<Testimonial[]>([])
+
+  ngOnInit(): void {
+    this.getTestimonials()
+  }
+
+
+  getTestimonials() {
+    const params = {
+      page: 1,
+      limit: 3
+    } as ExternalParams
+
+    this._testimonialsService.getTestimonials(params).subscribe({
+      next: (res: TestimonialsList) => {
+        this.testimonials.set(res.payload.data)        
+      }
+    })
+  }
+}
