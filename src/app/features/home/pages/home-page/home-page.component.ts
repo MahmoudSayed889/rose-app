abf8b9bf0abf86bbbff13686e0a061ea0a02794f
Othman Gamal <othman_gamal20@yahoo.com>
2026-07-01 15:20:34 +0300

    fix: resolve conflicts between products-page and service-section

--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { AboutUsComponent } from "../../components/about-us/about-us.component";
 import { GallerySectionComponent } from '../../components/gallery-section/gallery-section.component';
 import { TestimonialsSectionComponent } from '../../components/testimonials-section/testimonials-section.component';
+import { ServiceSectionComponent } from '../../components/Service Highlights-section/service-section/service-section.component';
+import { AboutUsComponent } from '../../components/about-us/about-us.component';
 
 @Component({
   selector: 'app-home-page',
   imports: [
     GallerySectionComponent,
     TestimonialsSectionComponent,
-    AboutUsComponent
+    ServiceSectionComponent,
+    AboutUsComponent,
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
