1630b29d4574bf29d86bdf9eabbc501b19cd58f3
MahmoudSayedDeve <76589982+MahmoudSayed889@users.noreply.github.com>
2026-07-02 03:51:02 +0300

    remove unused functions

--- src/app/features/home/components/testimonials-section/services/testimonials.service.ts
+++ src/app/features/home/components/testimonials-section/services/testimonials.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AUTH_API_URL } from 'auth-library';
-import { CreateTestimonialRequest, DeleteTestimonialResponse, SingleTestimonial, TestimonialsList } from '../models/testimonial';
+import { TestimonialsList } from '../models/testimonial';
 import { ExternalParams } from '../../../../../shared/models/external-params';
 import { HelperService } from '../../../../../shared/services/helper.service';
 
@@ -15,21 +15,5 @@ export class TestimonialsService {
 
     getTestimonials(params?: ExternalParams): Observable<TestimonialsList> {
         return this._httpClient.get<TestimonialsList>(`${this.baseUrl}/api/testimonials`, {params: this._helperService.createParams(params)})
-    }
-
-    getTestimonial(id: string): Observable<SingleTestimonial> {
-        return this._httpClient.get<SingleTestimonial>(`${this.baseUrl}/api/testimonials/${id}`)
-    }
-
-    createTestimonial(data: CreateTestimonialRequest): Observable<SingleTestimonial> {
-        return this._httpClient.post<SingleTestimonial>(`${this.baseUrl}/api/testimonials`, data)
-    }
-
-    updateTestimonial(id: string, data: CreateTestimonialRequest): Observable<SingleTestimonial> {
-        return this._httpClient.put<SingleTestimonial>(`${this.baseUrl}/api/testimonials/${id}`, data)
-    }
-
-    deleteTestimonial(id: string): Observable<DeleteTestimonialResponse> {
-        return this._httpClient.delete<DeleteTestimonialResponse>(`${this.baseUrl}/api/testimonials/${id}`)
     }
 }
