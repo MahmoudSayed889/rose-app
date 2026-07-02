9aebbbca405c0a6d217f40a3be1414c3cfb94517
MahmoudSayedDeve <76589982+MahmoudSayed889@users.noreply.github.com>
2026-07-02 03:41:46 +0300

    Use for loop with gallery items

--- src/app/features/home/components/gallery-section/gallery-section.component.ts
+++ src/app/features/home/components/gallery-section/gallery-section.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-gallery-section',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './gallery-section.component.html',
   styleUrl: './gallery-section.component.scss',
 })
-export class GallerySectionComponent {}
+export class GallerySectionComponent {
+
+  galleryItems = signal([
+    {
+      img: 'assets/imgs/gallery/g-1.png',
+      class: 'row-span-8',
+    },
+    {
+      img: 'assets/imgs/gallery/g-6.png',
+      class: 'row-span-4',
+    },
+    {
+      img: 'assets/imgs/gallery/g-5.png',
+      class: 'row-span-4',
+    },
+    {
+      img: 'assets/imgs/gallery/g-3.png',
+      class: 'row-span-4 lg:row-span-8',
+    },
+    {
+      img: 'assets/imgs/gallery/g-4.png',
+      class: 'row-span-20 lg:row-span-8',
+    },
+    {
+      img: 'assets/imgs/gallery/g-2.png',
+      class: 'row-span-16 lg:row-span-4',
+    },
+  ])
+}
