acde39d0fef6ded50b0dfaef95e92b08ad201d37
Othman Gamal <othman_gamal20@yahoo.com>
2026-07-02 11:34:50 +0300

    fix translate and array

--- src/app/features/home/components/trusted-by-section/trusted-by/trusted-by.component.ts
+++ src/app/features/home/components/trusted-by-section/trusted-by/trusted-by.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-trusted-by',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './trusted-by.component.html',
   styleUrl: './trusted-by.component.scss',
 })
-export class TrustedByComponent {}
+export class TrustedByComponent {
+  readonly companyLogos = [
+    'assets/imgs/image36.svg',
+    'assets/imgs/image40.svg',
+    'assets/imgs/image41.svg',
+    'assets/imgs/image38.svg',
+    'assets/imgs/image39.svg',
+    'assets/imgs/image37.svg',
+  ];
+}
