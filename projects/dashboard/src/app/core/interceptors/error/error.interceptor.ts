cd9ce2d88abb83702d85c0661fcd83969215be2b
MahmoudSayed889 <msayedm889@gmail.com>
2026-09-03 13:46:46 +0300

    Update error.interceptor.ts

--- projects/dashboard/src/app/core/interceptors/error/error.interceptor.ts
+++ projects/dashboard/src/app/core/interceptors/error/error.interceptor.ts
@@ -3,24 +3,28 @@ import { inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 import { ToastService } from '../../../shared/services/toast.service';
+import { environment } from '../../../../environments/environment';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  
+
   const _toastService = inject(ToastService)
   const _router = inject(Router)
 
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
 
-      // if (error.error?.message.includes('Invalid or expired token')) {
-      //   _router.navigate(['/login'])
-      //   return throwError(() => error);
-      // }
-      
+      if (error.error?.message.includes('Invalid or expired token')) {
+        const authUrl = new URL('/login', environment.hostUrl)
+        authUrl.searchParams.set('callbackurl', window.location.href)
+
+        window.location.href = authUrl.toString();
+        return throwError(() => error);
+      }
+
       const errorMessage = error.error?.message || 'حدث خطأ غير متوقع، يرجى المحاولة لاحقاً';
-      
+
       _toastService.toaster('error', errorMessage)
-      
+
       return throwError(() => error);
     })
   )
